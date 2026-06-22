1a4cf5c53915a8499c304e01a7299902b796785a
Ikra <ikrashaikh@p99soft.com>
2026-06-22 23:50:03 +0530

    Added Builder

--- src/main/java/com/example/BookStore/model/BookModel.java
+++ src/main/java/com/example/BookStore/model/BookModel.java
@@ -2,6 +2,7 @@ package com.example.BookStore.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
  */
 @Schema(description = "Represents a book in the BookStore catalogue")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookModel {
